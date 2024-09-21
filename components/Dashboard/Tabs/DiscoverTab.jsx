a88d1b918b59e9a6307866457e0faf31859ce75d
Aldo Barba <51721235+Aldobc1@users.noreply.github.com>
2024-09-21 15:45:10 +0800

    Update DiscoverTab.jsx

--- components/Dashboard/Tabs/DiscoverTab.jsx
+++ components/Dashboard/Tabs/DiscoverTab.jsx
@@ -78,7 +78,7 @@ const TEST_DATA = [
 ]
 
 const DiscoverTab = () => {
-  const isTest = false;
+  const isTest = true;
   const [videos, setVideos] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentVideoId, setCurrentVideoId] = useState(null);
